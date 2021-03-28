e34783996f907f752826f45cef60f916fdbd7197
Gowtamkumar <gowtampaul0@gmail.com>
2021-03-28 14:10:36 +0600

    port update for heroku

--- index.js
+++ index.js
@@ -63,17 +63,8 @@ client.connect(err => {
 
 
 
-
-
-
-
-
-
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
