a84e2933e68ddc28934699c94323ba2bcf5653ac
Gowtamkumar <gowtampaul0@gmail.com>
2021-03-28 14:21:01 +0600

    some change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ty6v7.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ty6v7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emajohnStore").collection("Products");
